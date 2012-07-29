354a15290f36e10dec2959c6c4c5aee3233c26d4
Palsan86 <apa_86@mail.ru>
2012-07-29 06:05:49 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leto::Application.config.secret_token = 'c19512dae5b7664e895d3aab0ede13148e8db259f558d35c5cc2caeb487e633e25e7b9bd1dba9da2f1bc4379a07f94c34f6c37365d785544c1de283c1d957bf8'
